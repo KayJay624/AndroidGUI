4eb617842ec2a27bb216ea5814a05fc18b58e886
Kamil Dawidów <kamil@dawidow.pl>
2016-12-05 23:35:44 +0100

    działający date_picker po DialogFragment

--- app/src/main/java/com/example/mobilne/simpletoolbar/MainActivity.java
+++ app/src/main/java/com/example/mobilne/simpletoolbar/MainActivity.java
@@ -1,64 +1,27 @@
 package com.example.mobilne.simpletoolbar;
 
-import android.os.Handler;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ProgressBar;
-import android.widget.RadioButton;
-import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.TimePicker;
-import android.widget.Toast;
 
-import org.w3c.dom.Text;
-
-import java.util.Calendar;
-
-public class MainActivity extends AppCompatActivity implements TimePicker.OnTimeChangedListener {
-    private TimePicker timePicker1;
-    private TextView time;
-    private Calendar calendar;
-    private String format = "";
+public class MainActivity extends AppCompatActivity {
+    public TextView time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        timePicker1 = (TimePicker) findViewById(R.id.timePicker1);
         time = (TextView) findViewById(R.id.textView);
-        calendar = Calendar.getInstance();
-
-        int hour = calendar.get(Calendar.HOUR_OF_DAY);
-        int min = calendar.get(Calendar.MINUTE);
-        showTime(hour, min);
-        timePicker1.setOnTimeChangedListener(this);
     }
 
-    public void showTime(int hour, int min) {
-        if (hour == 0) {
-            hour += 12;
-            format = "AM";
-        } else if (hour == 12) {
-            format = "PM";
-        } else if (hour > 12) {
-            hour -= 12;
-            format = "PM";
-        } else {
-            format = "AM";
-        }
-        time.setText(new StringBuilder().append(hour).append(" : ").append(min)
-                .append(" ").append(format));
+    public void showDatePickerDialog(View view) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 
-    @Override
-    public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
-        showTime(hourOfDay, minute);
+    public void setDate(int year, int month, int day) {
+        time.setText("Set date: " + year + "/" + (month + 1) + "/" + day);
     }
 }
