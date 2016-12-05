08a87ce8b470241ecb100e3c080d868c61ed9497
Kamil Dawidów <kamil@dawidow.pl>
2016-12-05 20:14:09 +0100

    początek progress bara

--- app/src/main/java/com/example/mobilne/simpletoolbar/MainActivity.java
+++ app/src/main/java/com/example/mobilne/simpletoolbar/MainActivity.java
@@ -11,29 +11,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (TextView)findViewById(R.id.textView);
-    }
-
-    public void onRadioButtonClicked(View view) {
-        // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
-
-        // Check which radio button was clicked
-        switch(view.getId()) {
-            case R.id.radio_pirates:
-                if (checked)
-                    textView.setText(R.string.pirates_chosen);
-                    break;
-            case R.id.radio_ninjas:
-                if (checked)
-                    textView.setText(R.string.ninjas_chosen);
-                    break;
-        }
     }
 }
