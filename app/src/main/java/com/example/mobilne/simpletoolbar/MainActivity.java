9095f202d5ed81ab4c30a148b1cfcddca2f30bab
Kamil Dawidów <kamil@dawidow.pl>
2016-12-05 20:39:04 +0100

    działa progress_bar

--- app/src/main/java/com/example/mobilne/simpletoolbar/MainActivity.java
+++ app/src/main/java/com/example/mobilne/simpletoolbar/MainActivity.java
@@ -1,20 +1,54 @@
 package com.example.mobilne.simpletoolbar;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+public class MainActivity extends AppCompatActivity {
+    private ProgressBar mProgress;
+    private int mProgressStatus = 0;
+
+    private Handler mHandler = new Handler();
+    private TextView textView;
+
+
+    protected void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
         setContentView(R.layout.activity_main);
+        textView = (TextView)findViewById(R.id.textView);
+
+        mProgress = (ProgressBar) findViewById(R.id.progress_bar);
+
+        // Start lengthy operation in a background thread
+        new Thread(new Runnable() {
+            public void run() {
+                while (mProgressStatus < 100) {
+                    mProgressStatus = doWork();
+
+                    // Update the progress bar
+                    mHandler.post(new Runnable() {
+                        public void run() {
+                            mProgress.setProgress(mProgressStatus);
+                        }
+                    });
+                }
+            }
+        }).start();
     }
+
+    private int doWork() {
+        android.os.SystemClock.sleep(50);
+        return mProgressStatus + 1;
+    }
+
 }
